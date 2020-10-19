07f83ddfa3ca7204083ddc41b36cd9d6dd5eb7fc
amkera <60901491+amkera@users.noreply.github.com>
2020-10-18 23:23:03 -0400

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,28 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ''
+    };
+  }
+  //1: in the beginning, set the state of an instance of a message equal to 0
+
+  handleChange = (event) => {
+    this.setState({
+      message: event.target.value
+    })
+    //as the user is typing, the message is going to be the event's target's value, which we set below
   }
 
   render() {
+    let charNumber = this.props.maxChars - this.state.message.length;
+    //this is the counter, it will decrease as you type because characters are being used
+    //when user types into the field, trigger the handleChange function and set the value to the message that's being typed
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" onChange={this.handleChange} value={this.state.message}/>
+        {charNumber}
       </div>
     );
   }
